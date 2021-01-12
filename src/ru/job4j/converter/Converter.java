407781d2a61dd99af89f7f217389800a9cffe889
ruslandavydov <ruslandav@rambler.ru>
2021-01-12 13:41:02 +0300

    correct syntax

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,4 +1,4 @@
-package ru.job4j.calculator;
+package ru.job4j.converter;
 
 public class Converter {
 
@@ -10,22 +10,10 @@ public class Converter {
         return value / 60;
     }
 
-    public static int euroToRuble(int value) {
-        return value * 70;
-    }
-
-    public static int dollarToRuble(int value) {
-        return value * 60;
-    }
-
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(120);
-        int rubleE = Converter.euroToRuble(1);
-        int rubleD = Converter.dollarToRuble(1);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("120 rubles are " + dollar + " dollars.");
-        System.out.println("1 euro is " + rubleE + " rubles.");
-        System.out.println("1 dollar is " + rubleD + " rubles.");
     }
 }
