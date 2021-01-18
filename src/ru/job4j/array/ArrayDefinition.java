c865b52437de385e9f5a04547fbaa0d5e73d2043
ruslandavydov <ruslandav@rambler.ru>
2021-01-18 12:09:31 +0300

    6.0.2. Заполнение массива. [#444129]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,9 +6,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
+        String[] names = new String[4];
+        names[0] = "Ruslan Davydov";
+        names[1] = "Anastasiia";
+        names[2] = "Mikhail";
+        names[3] = "David";
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
