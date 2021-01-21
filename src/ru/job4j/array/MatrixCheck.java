8aa3f72d3e2495724583f2d0114abb9ffe1e6e32
ruslandavydov <ruslandav@rambler.ru>
2021-01-21 13:03:30 +0300

    MatrixCheck with One cycle

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,12 +4,10 @@ public class MatrixCheck {
 
     public static boolean monoHorizontal(char[][] board, int line) {
         boolean result = true;
-        for (int row = 0; row < board.length; row++) {
-            for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[line][cell] != 'X') {
-                    result = false;
-                    break;
-                }
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[line][cell] != 'X') {
+                result = false;
+                break;
             }
         }
         return result;
@@ -18,11 +16,9 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int row = 0; row < board.length; row++) {
-            for (int cell = column; cell < board[row].length; cell++) {
-                if (board[row][column] != 'X') {
-                    result = false;
-                    break;
-                }
+            if (board[row][column] != 'X') {
+                result = false;
+                break;
             }
         }
         return result;
